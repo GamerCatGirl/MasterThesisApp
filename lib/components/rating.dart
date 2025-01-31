9ed210d1b85bd22446eae29da2add3dbaa84bff5
Silken <silken_heynderickx@icloud.com>
2025-01-31 12:07:47 +0100

    add rating

---
+++ lib/components/rating.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+class Rating extends StatefulWidget {
+  final String question;
+  final void Function(double) onChangeRating;
+
+  const Rating(
+      {super.key, required this.question, required this.onChangeRating});
+
+  @override
+  State<Rating> createState() => _RatingState();
+}
+
+class _RatingState extends State<Rating> {
+  @override
+  Widget build(BuildContext context) {
+    final ratingWidget = RatingBar.builder(
+        itemCount: 5,
+        itemSize: 30,
+        itemBuilder: (context, _) => Icon(
+              Icons.star,
+              color: Colors.amber,
+            ),
+        onRatingUpdate: (rating) {
+          widget.onChangeRating(rating);
+        });
+
+    return Row(
+      children: [
+        Spacer(),
+        Text(widget.question),
+        ratingWidget,
+        Spacer(),
+        Spacer()
+      ],
+    );
+  }
+}
--- lib/pages/signIn.dart
+++ lib/pages/signIn.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mathapp/components/rating.dart';
+import 'package:mathapp/components/title.dart';
 
 class Signin extends StatefulWidget {
   const Signin({super.key});
@@ -10,11 +12,26 @@ class Signin extends StatefulWidget {
 class _SignInState extends State<Signin> {
   int selectedPage = 0;
 
+  final TextEditingController username = TextEditingController();
+  final TextEditingController password = TextEditingController();
+  final TextEditingController repeatPassword = TextEditingController();
+
+  int answerQ1 = 0;
+
+  final String intro =
+      "Om zo goed mogelijk oefeningen aan te bieden op jouw niveau volgt een kleine vragenlijst. De antwoorden worden enkel gebruikt voor oefeningen op maat aan te bieden, de antwoorden zijn niet zichtbaar voor medeleering of leerkracht.";
+
+  final String question1 = "Q1: ";
+
   @override
   Widget build(BuildContext context) {
+    final q1 = Rating(question: question1, onChangeRating: (rating) {});
     final signInLogIn = Center(child: Text("Sign In Page"));
+    final page = ListView(
+      children: [Header(title: "Sign In"), Text(intro), q1],
+    );
 
-    final List _pages = [signInLogIn];
+    final List _pages = [page];
 
     return Scaffold(
       body: Center(child: _pages[selectedPage]),
