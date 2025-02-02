d5e22a1794a0dfac3717d002d79da4b30e9f5d0d
Silken <silken_heynderickx@icloud.com>
2025-02-02 13:13:03 +0100

    update packages

--- lib/pages/viewSkills.dart
+++ lib/pages/viewSkills.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mathapp/components/title.dart';
-//import 'package:fl_chart/fl_chart.dart';
+//import 'package:syncfusion_flutter_charts/charts.dart';
+//import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class Viewskills extends StatefulWidget {
   const Viewskills({super.key});
@@ -9,50 +10,18 @@ class Viewskills extends StatefulWidget {
   State<Viewskills> createState() => _SkillState();
 }
 
-class RawDataSet {
-  RawDataSet({
-    required this.title,
-    required this.color,
-    required this.values,
-  });
-
-  final String title;
-  final Color color;
-  final List<double> values;
-}
-
 class _SkillState extends State<Viewskills> {
   int selectedPage = 0;
 
-  RawDataSet rawDataSet = RawDataSet(
-    title: 'Fashion',
-    color: Colors.red,
-    values: [
-      300,
-      50,
-      250,
-    ],
-  );
-
   @override
   Widget build(BuildContext context) {
-    /*
-    final radar = RadarDataSet(
-      fillColor: rawDataSet.color.withAlpha(100),
-      borderColor: rawDataSet.color,
-      entryRadius: 3,
-      dataEntries: rawDataSet.values.map((e) => RadarEntry(value: e)).toList(),
-      borderWidth: 2.3,
-    );
-    */
-
     final page = ListView(
       children: [
         Header(title: "View Skills"),
         Text("test"),
-        //RadarChart(RadarChartData(dataSets: [radar])),
         Text("Lmao"),
         Text("add"),
+        Text("Lol")
       ],
     );
     final List _pages = [page];
